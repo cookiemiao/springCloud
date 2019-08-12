9499cf03a41dcb679d9316303053274d4d6c24b7
bisheng <Cookie5546511958>
2019-08-12 16:41:10 +0800

    sentinel 锚点限流配置

--- consumer/src/main/java/com/bs/producer/controller/HelloWorldController.java
+++ consumer/src/main/java/com/bs/producer/controller/HelloWorldController.java
@@ -1,8 +1,9 @@
 package com.bs.producer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.bs.producer.hello.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,31 @@ public class HelloWorldController {
     @Autowired
     HelloWorldService helloWorldService;
 
+    /**
+     * helloWorld
+     *
+     * @param name
+     * @return
+     */
     @GetMapping("helloWorld")
-    public String helloWorld(String name) {
+    @SentinelResource(value = "helloWorld", blockHandler = "helloWorldFallback")
+    public String helloWorld(String name) throws Exception{
+        // 为了使返回时间按比较长
+//        Thread.sleep(50);
         name = Optional.ofNullable(name).orElse("noName");
         return helloWorldService.helloWorld(name);
+    }
+
+    /**
+     * helloWorld限流处理,入参必须一致，最后再加上BlockException,返回类型也必须一样
+     *
+     * @param name
+     * @param ex
+     * @return
+     */
+    public String helloWorldFallback(String name, BlockException ex){
+
+        return "helloWorldFallback";
     }
 
 }
